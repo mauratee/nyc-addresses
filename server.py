234825d841e13b9dda190e1183faa257b5b44376
mauratee <thomaura@gmail.com>
2024-12-11 16:14:03 -0500

    add missing files

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/search-handling", methods=['POST'])
+def search_handling():
+    text = request.form['address_search']
+    print(text)
+    return text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
